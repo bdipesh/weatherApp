9cfe0a86dce292a710644fc9f248a4a62506be70
Dipesh <bdipesh08@gmail.com>
2018-08-08 07:03:19 +0545

    updated

--- weather/src/components/HelloWorld.vue
+++ weather/src/components/HelloWorld.vue
@@ -2,39 +2,57 @@
 
 
 
-  <v-container fluid>
+  <v-container fluid class="wraper">
        <v-text-field
             label="EnterCity"
             v-model="address"
           ></v-text-field>
+    {{address}}
 
-
-      <v-list>{{getDate()}}</v-list>
-       <v-list>{{place}}</v-list>
-
-      <v-list> <v-layout><v-flex><img :src="icon">{{temperature}}</v-flex><v-flex>{{description}}</v-flex></v-layout></v-list>
+<v-container>
+  <h1>{{place}}</h1>
+  <h2>{{getDate()}}</h2>
+  <h2>{{description}}</h2>
+  <v-layout>
+  <v-flex><img :src="icon"><h1>{{temperature}} C</h1></v-flex>
+  </v-layout>
+  </v-container>
 
 
 
     <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
 
-        <blockquote>
 
-          <footer>
-            <small>
-              <em>{{info}}</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
     </v-slide-y-transition>
+    <v-flex xs12 sm6 class="py-2">
+
+      <v-btn-toggle>
+        <v-btn flat value="left">
+          Temperature
+        </v-btn>
+        <v-btn flat value="center">
+          Percipitation
+        </v-btn>
+        <v-btn flat value="right">
+          Wind
+        </v-btn>
+
+
+      </v-btn-toggle>
+    </v-flex>
   </v-container>
 </template>
 
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style >
+ v-list{
+   margin:0px;
+   padding:0px;
+ }
 h1, h2 {
+  padding: 0px;
+  margin:0px;
   font-weight: normal;
 }
 ul {
@@ -48,7 +66,13 @@ li {
 a {
   color: #42b983;
 }
+  .wraper{
+    width:500px;
+    height:500px;
+    border: black 2px solid;
+  }
 </style>
+
 <script>
 import axios from 'axios'
 
@@ -56,16 +80,16 @@ export default {
   data () {
     return {
       getDate () {
-        const toTwoDigits = num => num < 10 ? '0' + num : num;
         let today = new Date()
-        let year = today.getFullYear()
-        let month = toTwoDigits(today.getMonth() + 1)
-        let day = toTwoDigits(today.getDate())
-        return `${year}-${month}-${day}`
+        let minutes = today.getMinutes()
+        let hour = today.getHours()
+        let weekday = ['sunday', 'monday', 'Tuesday', 'wednesday', 'Thrusday', 'Friday', 'Saturday']
+        let day = weekday[today.getDay()]
+        return `${day} ${hour} : ${minutes}`
       },
       info: null,
       temperature: '',
-      currentLocation: { lat: 0, lng: 0},
+      currentLocation: {lat: 0, lng: 0},
       searchAddressInput: '',
       async: true,
       place: '',
@@ -75,40 +99,16 @@ export default {
       address: ''
     }
   },
-  created () {
-    axios.get('https://fcc-weather-api.glitch.me/api/current?lat=27.688495000000003&lon=85.33945489999999')
-      .then((response) => {
-        this.info = response.data
-        this.place = this.info.name
-        this.icon = this.info.weather[0].icon
-        this.description = this.info.weather[0].description
-        this.temperature = this.info.main.temp
-      })
-    axios.get('https://maps.googleapis.com/maps/api/js?sensor=false')
-        .then((callback, address) => {
-          address = address || 'kathamadu'
-          geocoder = new google.maps.Geocoder();
-        })
-  },
-  mounted: function () {
-    this.geolocation()
-  },
-
-  methods: {
-    geolocation: function () {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.currentLocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
-      })
-    },
-    getLatitudeLongitude: function (callback, address) {
+  mounted () {
+    let goglescript = document.createElement('script')
+    goglescript.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false')
+    document.head.appendChild(goglescript)
+    function getlatlon (callback, address) {
       address = address || 'Kathamandu'
       geocoder = new google.maps.Geocoder()
       if (geocoder) {
         geocoder.geocode({
-            'address': address
+          'address': address
         }, function (results, status) {
           if (status == google.maps.GeocoderStatus.OK) {
             callback(results[0]);
@@ -116,7 +116,17 @@ export default {
         })
       }
     }
-
+  },
+  created () {
+    axios.get('https://fcc-weather-api.glitch.me/api/current?lat=27.7172453&lon=85.3239605')
+      .then((response) => {
+        this.info = response.data
+        this.place = this.info.name
+        this.icon = this.info.weather[0].icon
+        this.description = this.info.weather[0].description
+        this.temperature = this.info.main.temp
+        this.pressure = this.info.
+      })
   }
 }
 </script>
